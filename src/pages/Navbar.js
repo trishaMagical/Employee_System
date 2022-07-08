1a02ec31d5b9d65266bbb5bdc3a2e87dc5558f15
trishaMagical <trisha.hazra@madgicaltechdom.com>
2022-07-08 18:18:49 +0530

    Icons created

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,14 +1,28 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import { Link } from "react-router-dom";
 import * as  FaIcons from "react-icons/fa";
+import * as  AiIcons from "react-icons/ai";
 const Navbar = () => {
+    const[sidebar, setSidebar] =useState(false);
+    const showSidebar = () => setSidebar(sidebar);
   return (
     <>
       <div className="navbar">
         <Link to ="#" className='menu-bars'>
-            <FaIcons.FaBars />
+            <FaIcons.FaBars onClick/>
         </Link>
       </div>
+      <nav className={sidebar ? 'nav-menu active': 'nav-menu'}> 
+        <ul className='nav-menu-items'>
+            <li className='navbar-toogle'>
+                <Link to='#' className='menu-bars'>
+                    <AiIcons.AiOutlineClose />
+                
+                </Link>
+            </li>
+        </ul>
+
+      </nav>
     </>
   )
 }
