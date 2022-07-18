26cbf17cc67f59ab0ff42829da4602655ed77976
trishaMagical <trisha.hazra@madgicaltechdom.com>
2022-07-18 15:26:05 +0530

    Update API with Id Done

---
+++ src/pages/Todo.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import TodoForm from "./TodoForm";
+import { RiCloseCircleLine } from "react-icons/ri";
+import { TiEdit } from "react-icons/ti";
+import axios from 'axios';
+import "./Todo.css";
+
+
+
+// import { IoIosDoneAll } from 'react-icons/io';
+
+const Todo = ({ todos, removeTodo, updateTodo }) => {
+  const [edit, setEdit] = useState({
+    id: null,
+    value: ""
+  });
+
+  const submitUpdate = (value) => {
+    updateTodo(edit.id, value);
+    setEdit({
+      id: null,
+      value: ""
+    });
+  };
+
+  // if (edit.id) {
+  //   return <TodoForm edit={edit} onSubmit={submitUpdate} />;
+  // }
+  console.log("Todos", todos);
+
+  return <form>
+    <div>
+    {
+      todos.map((todo, index) => (
+        <React.Fragment>
+          {edit.id && edit.id === todo.id ?
+            <TodoForm edit={edit} onSubmit={submitUpdate}
+              data={todos}
+
+            />
+            // console.log("data", data);
+            : <div
+              className={todo.isComplete ? "todo-row complete" : "todo-row"}
+              key={index}
+            >
+              <div key={todo.id} >
+                {todo.text}
+              </div>
+              <div className="icons">
+                <RiCloseCircleLine
+                  onClick={() => removeTodo(todo.id)}
+                  className="delete-icon"
+                />
+                <TiEdit
+                  onClick={() => setEdit({ id: todo.id, value: todo.text })}
+                  className="edit-icon"
+                />
+              </div>
+
+            </div>
+          }
+        </React.Fragment>
+      ))
+      
+    }
+ {/* <button className="button" onClick={handleSave}>Save To DataBase</button> */}
+  
+  </div>
+  </form>
+
+};
+
+export default Todo;
--- src/pages/TodoList.js
+++ src/pages/TodoList.js
@@ -1,16 +1,86 @@
-import React,{useState} from 'react';
-const initialState ={
-  todolist:""
+import React, { useEffect, useState } from "react";
+import TodoForm from "./TodoForm";
+import Todo from "./Todo";
+import axios from "axios";
+import "./Todo.css";
 
+function TodoList() {
+  const [todos, setTodos] = useState([]);
+  useEffect  (  ()=>{
+    const data = JSON.parse(localStorage.getItem("userInfo"));
+    console.log("data",data);
+    axios
+    .get(`http://localhost:5000/api/todoSaved/${data.email}`)
+    .then(response => {
+      console.log("response",response.data[0].todolist);
+      // console.log("response",response.data, JSON.parse(response.data));
+      setTodos(JSON.parse(response.data[0].todolist));
+
+    })
+     .catch(err=>{
+        console.log(err);
+    })
+    
+
+   
+},0)
+  const addTodo = (todo) => {
+    if (!todo.text || /^\s*$/.test(todo.text)) {
+      return;
+    }
+
+    const newTodos = [todo, ...todos];
+
+    setTodos(newTodos);
+    console.log("NewTodos", newTodos);
+    handleSave(newTodos);
+    console.log(...todos);
+
+  };
+const handleSave = async(newTodos) =>{
+  
+    const data = JSON.parse(localStorage.getItem("userInfo"));
+   console.log("data",data);
+    
+    axios
+    .put(`http://localhost:5000/api/todolist/${data.email}`,
+ {todolist:JSON.stringify(newTodos)},
+    )
+    
+    
 }
+  const updateTodo = async (todoId, newValue) => {
+    if (!newValue.text || /^\s*$/.test(newValue.text)) {
+      return;
+    }
 
-const TodoList = () => {
-  const [state, setState]= useState(initialState);
-  const {todolist} = state;
+    setTodos((prev) =>
+      prev.map((item) => (item.id === todoId ? newValue : item))
+    );
+  console.log("Todos123",todos);
+  //await handleSave();
+  };
+
+  const removeTodo = (id) => {
+    const removedArr = [...todos].filter((todo) => todo.id !== id);
+    
+    setTodos(removedArr);
+    handleSave(removedArr);
+  };
+
+  const completeTodo = (id) => {
+    let updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
 
   return (
     <>
-     <nav className="navbar navbar-expand-lg navbar-light bg-danger ">
+      <nav className="navbar navbar-expand-lg navbar-light bg-danger ">
         <a className="navbar-brand text-white" href="#">Welcome</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
@@ -31,13 +101,15 @@ const TodoList = () => {
         </div>
       </nav>
       <h1>What's the Plan for Today?</h1>
-      <input type="text"
-        id="todolist"
-        name="todolist"
-        placeholder="Your Todo's...." value={todolist } />
-    
+      <TodoForm onSubmit={addTodo} />
+      <Todo
+        todos={todos}
+        // completeTodo={completeTodo}
+        removeTodo={removeTodo}
+        updateTodo={updateTodo}
+      />
     </>
-  )
+  );
 }
 
-export default TodoList
+export default TodoList;
